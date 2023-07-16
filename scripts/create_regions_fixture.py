262649b5f639128b022ada48128060962cf7b8c7
seylu <98249191+seyLu@users.noreply.github.com>
2023-07-17 03:18:02 +0800

    feat: start adding asyncio to script

--- scripts/create_regions_fixture.py
+++ scripts/create_regions_fixture.py
@@ -29,20 +29,88 @@ __version__ = "0.0.1"
 __maintainer__ = "seyLu"
 __status__ = "Prototype"
 
+import asyncio
 import json
-import os
 
 import requests
 
 REGION_VIII_CODE = "080000000"
-CITY_OF_TACLOBAN_CODE = "0831600000"
+CITY_OF_TACLOBAN_CODE = "083747000"
 
 BASE_URL = "https://psgc.gitlab.io/api"
 BASE_PATH = "results"
 
+API_MAP = {
+    "regions": "regions",
+    "provinces": "provinces",
+    "cities": "cities-municipalities",
+    "districts": "barangays",
+}
 
-def main():
-    _()
+FIXTURE_JSON_MAP = {
+    "model": None,
+    "pk": None,
+    "fields": {
+        "name": None,
+        "code": None,
+    },
+}
+
+
+async def main():
+    regions, province_codes = await get_regions()
+    # provinces, city_codes = get_provinces(province_codes)
+    # cities, district_codes = get_cities(city_codes)
+    # districts = get_districts(district_codes)
+
+    print(f"{regions=}\n{province_codes=}")
+
+
+async def get_regions(codes: list[str] | None = None) -> tuple:
+    """Get all regions or select regions from api."""
+
+    regions: list[dict]
+    fixtures: list[dict] = []
+    province_codes: list[str] = []
+
+    MODEL_NAME: str = "regions.region"
+    BASE_ENDPOINT: str = f"{BASE_URL}/regions"
+
+    if not codes:
+        regions = requests.get(BASE_ENDPOINT).json()
+        codes = [region["code"] for region in regions]
+
+    for pk, code in enumerate(codes, start=1):
+        fixture, province_code_list = await get_region(
+            BASE_ENDPOINT=BASE_ENDPOINT, MODEL_NAME=MODEL_NAME, code=code, pk=pk
+        )
+
+        fixtures.append(fixture)
+        province_codes.extend(province_code_list)
+
+    return fixtures, province_codes
+
+
+async def get_region(BASE_ENDPOINT, MODEL_NAME, code, pk):
+    """Get a single region from api."""
+
+    endpoint = f"{BASE_ENDPOINT}/{code}"
+    region = requests.get(endpoint).json()
+
+    fixture = {
+        "model": MODEL_NAME,
+        "pk": pk,
+        "fields": {
+            "name": region["name"],
+            "code": region["code"],
+        },
+    }
+
+    provinces_endpoint = f"{endpoint}/provinces"
+    provinces = requests.get(provinces_endpoint).json()
+    province_code_list = [province["code"] for province in provinces]
+
+    return fixture, province_code_list
 
 
 def _() -> None:
@@ -108,4 +176,4 @@ def _() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
