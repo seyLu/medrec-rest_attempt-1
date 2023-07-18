021459b6b1e50e05d620207defd2fa09ca829dc8
seylu <98249191+seyLu@users.noreply.github.com>
2023-07-19 01:34:33 +0800

    feat: improve api request count from 4500+ to 4

--- scripts/create_regions_fixture.py
+++ scripts/create_regions_fixture.py
@@ -30,11 +30,13 @@ __maintainer__ = "seyLu"
 __status__ = "Prototype"
 
 import asyncio
-import itertools
-import logging
+import yaml
+import os
 from logging.config import fileConfig
 
 import aiohttp
+
+import json
 
 REGION_VIII_CODE = "080000000"
 CITY_OF_TACLOBAN_CODE = "083747000"
@@ -63,111 +65,115 @@ API_PARENT_MAP = {
     "districts": "cities",
 }
 
-
-class PsgcAPI:
-    def __init__(self, endpoint):
-        self.base_endpoint: str = f"{BASE_URL}/{API_MAP[endpoint]}"
-        self.child_endpoint: str | None = API_MAP.get(API_CHILD_MAP[endpoint])
-        self.parent: str = API_PARENT_MAP[endpoint]
-        self.model_name: str = f"regions.{endpoint}"
-        self.items: list[dict] = []
-        self.fixtures: list[dict] = []
-        self.parent_child_codes: list[tuple[str, str]] = []
-
-    async def list(
-        self, session, parent_child_codes: list[tuple[str, str]] | None = None
-    ):
-        """Get all json items or select json items from api."""
-
-        parent_codes: list[str] | None = None
-
-        if not parent_child_codes:
-            async with session.get(f"{self.base_endpoint}.json") as response:
-                logging.info(f"Requesting {response.url}.")
-                items = await response.json()
-                codes = [item["code"] for item in items]
-        else:
-            parent_codes, codes = zip(*parent_child_codes)
-
-        for pk, code in enumerate(codes, start=1):
-            endpoint = f"{self.base_endpoint}/{code}"
-            async with session.get(f"{endpoint}.json") as response:
-                logging.info(f"Requesting {response.url}.")
-                item = await response.json()
-
-                fixture = {
-                    "model": self.model_name,
-                    "pk": pk,
-                    "fields": {
-                        "name": item["name"],
-                        "code": item["code"],
-                    },
-                }
-
-                if parent_codes:
-                    if parent_code := parent_codes[pk - 1]:
-                        fixture["fields"][f"{self.parent}_code"] = parent_code
-
-                self.fixtures.append(fixture)
-
-        return self.fixtures
-
-    async def list_child_codes(self, session, code: str):
-        """List child item codes."""
-
-        endpoint = f"{self.base_endpoint}/{code}"
-        child_endpoint = f"{endpoint}/{self.child_endpoint}"
-        async with session.get(f"{child_endpoint}.json") as response:
-            logging.info(f"Requesting {response.url}.")
-            child_items = await response.json()
-
-            self.parent_child_codes = list(
-                zip(
-                    itertools.repeat(code),
-                    [child_item["code"] for child_item in child_items],
-                )
-            )
-
-        return self.parent_child_codes
+SAMPLE_FIXTURE_MAP = [
+    {
+        "model": "regions.province",
+        "pk": "1",
+        "fields": {
+            "region_code": "0800000000",
+            "name": "City of Tacloban",
+            "code": "0831600000",
+        },
+    }
+]
 
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        regions_obj = PsgcAPI("regions")
-        provinces_obj = PsgcAPI("provinces")
-        cities_obj = PsgcAPI("cities")
-        districts_obj = PsgcAPI("districts")
+        region_viii_regions_url: str = await get_api_map(
+            BASE_URL, f"regions/{REGION_VIII_CODE}"
+        )
+        region_viii_provinces_url: str = await get_api_map(
+            region_viii_regions_url, "provinces"
+        )
+        region_viii_cities_url: str = await get_api_map(
+            region_viii_regions_url, "cities"
+        )
+        region_viii_districts_url: str = await get_api_map(
+            region_viii_regions_url, "districts"
+        )
 
-        region_fixtures = await regions_obj.list(session, [(None, REGION_VIII_CODE)])
-        region_province_codes = [
-            region_province_code
-            for region_fixture in region_fixtures
-            for region_province_code in await regions_obj.list_child_codes(
-                session, region_fixture["fields"]["code"]
-            )
-        ]
+        region_viii_regions_json: dict = await get_json_response(session, f"{region_viii_regions_url}.json")
+        region_viii_provinces_json: dict = await get_json_response(session, region_viii_provinces_url)
+        region_viii_cities_json: dict = await get_json_response(session, region_viii_cities_url)
+        region_viii_districts_json: dict = await get_json_response(session, region_viii_districts_url)
 
-        province_fixtures = await provinces_obj.list(session, region_province_codes)
-        province_city_codes = [
-            province_city_code
-            for province_fixture in province_fixtures
-            for province_city_code in await provinces_obj.list_child_codes(
-                session, province_fixture["fields"]["code"]
-            )
-        ]
+        region_fixtures: dict = await get_fixtures("region", region_viii_regions_json)
+        province_fixtures: dict = await get_fixtures("province", region_viii_provinces_json)
+        city_fixtures: dict = await get_fixtures("city", region_viii_cities_json)
+        district_fixtures: dict = await get_fixtures("district", region_viii_districts_json)
 
-        city_fixtures = await cities_obj.list(session, province_city_codes)
-        city_district_codes = [
-            city_district_code
-            for city_fixture in city_fixtures
-            for city_district_code in await cities_obj.list_child_codes(
-                session, city_fixture["fields"]["code"]
-            )
-        ]
+        with open(
+            os.path.join(BASE_PATH, "Region.yaml"), "w+"
+        ) as f:
+            print(get_yaml(region_fixtures), file=f)
 
-        district_fixtures = await districts_obj.list(session, city_district_codes)
+        with open(
+            os.path.join(BASE_PATH, "Province.yaml"), "w+"
+        ) as f:
+            print(get_yaml(province_fixtures), file=f)
 
-        print(f"{district_fixtures=}")
+        with open(
+            os.path.join(BASE_PATH, "City.yaml"), "w+"
+        ) as f:
+            print(get_yaml(city_fixtures), file=f)
+
+        with open(
+            os.path.join(BASE_PATH, "District.yaml"), "w+"
+        ) as f:
+            print(get_yaml(district_fixtures), file=f)
+
+
+
+async def get_api_map(url: str, endpoint: str) -> str:
+    if api_endpoint := API_MAP.get(endpoint):
+        url += f"/{api_endpoint}.json"
+    else:
+        url += f"/{endpoint}"
+
+    return f"{url}"
+
+
+async def get_json_response(session, url: str):
+    async with session.get(url) as response:
+        if response.content_type != "application/json":
+            raise ValueError(f"Unexpected content type: {response.content_type}")
+
+        return await response.json()
+
+
+async def get_fixtures(name: str, items: list[dict]) -> list[dict]:
+    if not isinstance(items, list):
+        items = [items]
+    return [await get_fixture(name, item, pk) for pk, item in enumerate(items, start=1)]
+
+
+async def get_fixture(name: str, item: dict, pk) -> dict:
+    fixture: dict = {
+        "model": f"regions.{name}",
+        "pk": pk,
+        "fields": {
+            "code": item["code"],
+            "name": item["name"],
+        }
+    }
+
+    if name != "region":
+        fixture["fields"]["region_code"] = item["regionCode"]
+
+        if name != "province":
+            fixture["fields"]["province_code"] = item["provinceCode"]
+
+            if name != "city":
+                if city_code := item.get("municipalityCode"):
+                    fixture["fields"]["city_code"] = city_code
+                fixture["fields"]["city_code"] = item["cityCode"]
+
+    return fixture
+
+
+def get_yaml(fixture: dict):
+    return yaml.dump(fixture, default_flow_style=False, sort_keys=False)
 
 
 if __name__ == "__main__":
