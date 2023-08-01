d94dcd47bdfab7bd5351166fee3d7d688183803b
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-01 15:45:37 +0800

    bugix: comment out django placeholder unused imports

--- api/views.py
+++ api/views.py
@@ -1,8 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-# Create your views here.
-
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
