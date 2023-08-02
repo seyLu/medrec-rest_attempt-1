6ae8da10aeb0483b96de54fc81558cfc4b8d8a42
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-02 13:29:05 +0800

    chore: remove args to as_view()

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,7 @@
 from django.urls import path
 
 from . import views
-from .models import User
-from .serializers import UserSerializer
 
 urlpatterns = [
-    path(
-        "users/",
-        views.UserListCreateAPIView.as_view(
-            queryset=User.objects.all(), serializer_class=UserSerializer
-        ),
-        name="user-list-create"
-    )
+    path("users/", views.UserListCreateAPIView.as_view(), name="user-list-create"),
 ]
