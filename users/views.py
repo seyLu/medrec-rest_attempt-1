834fc617a7a84e36069bdf18a8e35784c6c59a30
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-03 08:59:55 +0800

    chore: update users lookup_field to uuid

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from .models import User
 from .serializers import UserSerializer
@@ -10,16 +11,16 @@ class UserListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = UserSerializer
 
     def list(self, request):
-        qs = self.get_queryset()
-        serializer = self.get_serializer_class()(qs, many=True)
+        serializer = self.get_serializer_class()(self.get_queryset(), many=True)
         return Response(serializer.data)
 
 
 class UserDetailApiView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    lookup_field = "uuid"
 
-    def retrieve(self, request, pk=None):
-        qs = self.get_queryset().filter(uuid=pk)
-        serializer = self.get_serializer_class()(qs, many=True)
+    def retrieve(self, request, uuid=None):
+        user = get_object_or_404(self.get_queryset(), uuid=uuid)
+        serializer = self.get_serializer_class()(user, many=True)
         return Response(serializer.data)
