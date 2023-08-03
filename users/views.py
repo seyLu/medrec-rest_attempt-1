5c3e025a37919b0b068249985dddfa9d4c02fc9d
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-03 09:00:13 +0800

    lint

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 
 from .models import User
 from .serializers import UserSerializer
