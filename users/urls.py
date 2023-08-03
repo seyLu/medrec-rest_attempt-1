68a746cb3151ab297c6e19cb6eeef62dea35a237
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-03 10:55:38 +0800

    feat: add urlpattern for retrieving indiv user

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("users/", views.UserListCreateAPIView.as_view(), name="user-list-create"),
+    path("users/<str:uuid>", views.UserDetailApiView.as_view(), name="user-detail"),
 ]
