fb9e47e9a39a65c64c5c1730f977ba887e46ada6
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-08 10:38:56 +0800

    feat: generate User.yaml with mocked user data

--- scripts/generate_users_fixture.py
+++ scripts/generate_users_fixture.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Using OPENAI
+Using Faker
 
 generate users fixture based on specified fields
 """
@@ -15,23 +15,42 @@ __maintainer__ = "seyLu"
 __status__ = "Prototype"
 
 
+import logging
 from logging.config import fileConfig
 
 import phonenumbers
 import yaml
 from faker import Faker
 
+BASE_PATH: str = "fixtures"
+MODEL_NAME: str = "user"
+YAML_FILENAME: str = f"{MODEL_NAME.upper()}.yaml"
+
 
 def main():
     fake = Faker(["en_PH"])
     Faker.seed(0)
 
+    logging.info(f"Generating {YAML_FILENAME}")
+    with open(f"{BASE_PATH}/User.yaml", "w+") as f:
+        print(
+            yaml.dump(
+                get_users_fixture(fake),
+                default_flow_style=False,
+                sort_keys=False,
+            ),
+            file=f,
+        )
+
+
+def get_users_fixture(fake: Faker) -> list[dict]:
+    logging.info(f"Serializing fixture {MODEL_NAME}")
     fixtures: list[dict] = []
     for pk in range(1, 101):
-        is_email_verified = fake.boolean(chance_of_getting_true=75)
+        is_email_verified: bool = fake.boolean(chance_of_getting_true=75)
         fixtures.append(
             {
-                "model": "models.user",
+                "model": f"models.{MODEL_NAME}",
                 "pk": pk,
                 "fields": {
                     "first_name": fake.first_name(),
@@ -48,16 +67,7 @@ def main():
                 },
             }
         )
-
-    with open("results/User.yaml", "w+") as f:
-        print(
-            yaml.dump(
-                fixtures,
-                default_flow_style=False,
-                sort_keys=False,
-            ),
-            file=f,
-        )
+    return fixtures
 
 
 if __name__ == "__main__":
