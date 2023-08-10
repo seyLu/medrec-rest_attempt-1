c94bddcc9082c1bd0f324f76879b4d2354d344a3
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-10 12:01:37 +0800

    feat: add register view

--- auth/models.py
+++ auth/models.py
@@ -1,1 +1,19 @@
-# Create your models here.
+from rest_framework import serializers
+
+from users.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    pass
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = [
+            "email",
+            "password",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
--- auth/views.py
+++ auth/views.py
@@ -1,8 +1,11 @@
-from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth import authenticate
+from rest_framework import serializers, status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-from users.models import User
+from auth.models import LoginSerializer
 from users.serializers import UserSerializer
 
 
@@ -15,15 +18,33 @@ class RegisterAPIView(APIView):
 
 
 class LoginAPIView(APIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
     def post(self, request):
-        email = request.data["email"]
-        password = request.data["password"]
+        serializer = self.serializer_class(data=request.data)
 
-        user = User.objects.filter(email=email).first()
+        if not serializer.is_valid():
+            return Response(
+                {
+                    "status": status.HTTP_400_BAD_REQUEST,
+                    "message": "Bad Request",
+                    "data": serializer.errors,
+                },
+                status.HTTP_400_BAD_REQUEST,
+            )
+
+        email = serializer.validated_data["email"]
+        password = serializer.validated_data["password"]
+
+        user = authenticate(request, email=email, password=password)
         if user is None:
-            raise AuthenticationFailed("User not found!")
+            raise serializers.ValidationError({"detail": "Invalid Email or Password!"})
 
-        if not user.check_password(password):
-            raise AuthenticationFailed("Incorrect password!")
-
-        return Response({"message": "Success!"})
+        refresh = RefreshToken.for_user(user)
+        return Response(
+            {
+                "token": str(refresh),
+            }
+        )
