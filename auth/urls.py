cb7735bde4af42fec6c3f9ced88f50a98c955e88
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-10 10:58:35 +0800

    feat: add login view

--- auth/urls.py
+++ auth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import RegisterAPIView
+from . import views
 
 urlpatterns = [
     path("auth/token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
-    path("auth/register", RegisterAPIView.as_view(), name="register"),
+    path("auth/register", views.RegisterAPIView.as_view(), name="register"),
+    path("auth/login", views.LoginAPIView.as_view(), name="login"),
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,6 +1,8 @@
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from users.models import User
 from users.serializers import UserSerializer
 
 
@@ -10,3 +12,18 @@ class RegisterAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data["email"]
+        password = request.data["password"]
+
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            raise AuthenticationFailed("User not found!")
+
+        if not user.check_password(password):
+            raise AuthenticationFailed("Incorrect password!")
+
+        return Response({"message": "Success!"})
