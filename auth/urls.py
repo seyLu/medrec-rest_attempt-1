f07640490b73cb71e2619421ee454c0c6cf590aa
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-10 10:23:47 +0800

    feat: add register view

--- auth/urls.py
+++ auth/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from .views import RegisterAPIView
+
 urlpatterns = [
     path("auth/token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("auth/register", RegisterAPIView.as_view(), name="register"),
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.serializers import UserSerializer
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
