62f8c5a714c5f41279473b101308e75d9b3336d8
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-12 00:32:53 +0800

    add url endpoints

--- auth/urls.py
+++ auth/urls.py
@@ -1,3 +1,9 @@
-# from django.urls import path
+from django.urls import path
 
-urlpatterns = []
+from . import views
+
+urlpatterns = [
+    path("auth/register/", views.RegisterAPIView.as_view(), name="register"),
+    path("auth/login/", views.LoginAPIView.as_view(), name="login"),
+    path("auth/logout/", views.LogoutAPIView.as_view(), name="logout"),
+]
